f8b83479cd3c5803f0b8b4dbb2d1b315bf3a9922
Dell <Dell@DESKTOP-0J4MMJA>
2020-07-07 17:20:04 +0530

    AroundAdvice-4

---
+++ AOPProj02-SpringAOPDecl-AroundAdvice-4/src/main/java/com/nt/advice/AroundLoggingAdvice1.java
@@ -0,0 +1,33 @@
+package com.nt.advice;
+
+import java.util.Arrays;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+public class AroundLoggingAdvice1 implements MethodInterceptor {
+
+	@Override
+	public Object invoke(MethodInvocation invocation) throws Throwable {
+		Object retVal=null;
+		Object args[]=null;
+		System.out.println("Entering Into "+invocation.getMethod().getName()+"with args"+Arrays.deepToString(invocation.getArguments()));
+		//To modify target method  arg value...
+		args=invocation.getArguments();
+		if(((Float)args[0])<50000)
+			args[1]=((Float)args[1])-0.5f;
+		
+		if(((Float)args[0])<0 || ((Float)args[1])<0 || ((Float)args[2])<0)
+			throw new IllegalArgumentException("Invalid Inputs");
+		else 
+			
+		retVal=invocation.proceed();
+
+		System.out.println("Existing from "+invocation.getMethod().getName()+"with args"+Arrays.deepToString(invocation.getArguments()));
+		//To modify target method return value....
+		retVal=((Float)retVal)+((Float)retVal*0.05f);
+		return retVal;
+	}
+
+	
+}
---
+++ AOPProj02-SpringAOPDecl-AroundAdvice-4/src/main/java/com/nt/service/BankServiceImpl.java
@@ -0,0 +1,16 @@
+package com.nt.service;
+
+public class BankServiceImpl implements IBankService {
+	// ptr/100
+		public float calcSimpleIntrestAmount(float pAmt,float rate,float time) {
+			System.out.println("BankService.calcSimpleIntrestAmount() Target Method1 "+"  "+pAmt+" " +rate+" "+time);
+			return (pAmt*rate*time)/100.0f;
+		}//method
+		
+		
+		//p(1+(r/100)^n -p
+		public   float calcCompoundIntrestAmount(float pAmt,float rate,float time) {
+			System.out.println("BankService.calcCompoundIntrestAmount()--Target method2"+"  "+pAmt+" " +rate+" "+time);
+			return (float) ((pAmt*Math.pow((1+rate/100),time)))-pAmt;
+		}//method
+}//class
